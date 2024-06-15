70474dcce2bb80c312b6e11c2045fc282dbb0fde
Tuan Ngo <126153408+kollersi-me@users.noreply.github.com>
2024-06-15 15:54:27 +0700

    Update [...path].tsx

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -13,15 +13,13 @@ export default function Folders() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
-      <Head>
-        <title>{siteConfig.title}</title>
-      </Head>
+
 
       <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
-        <Navbar />
+       
         <div className="mx-auto w-full max-w-5xl py-4 sm:p-4">
           <nav className="mb-4 flex items-center justify-between space-x-3 px-4 sm:px-0 sm:pl-1">
-            <Breadcrumb query={query} />
+         
             <SwitchLayout />
           </nav>
           <FileListing query={query} />
