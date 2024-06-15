fe5511e039da880b64cbfbaa9adfcddd0c07d663
Tuan Ngo <126153408+kollersi-me@users.noreply.github.com>
2024-06-15 18:27:12 +0700

    Update DownloadBtnGtoup.tsx

--- src/components/DownloadBtnGtoup.tsx
+++ src/components/DownloadBtnGtoup.tsx
@@ -79,22 +79,6 @@ const DownloadButtonGroup = () => {
           btnIcon="file-download"
           btnTitle={'Download the file directly through OneDrive'}
         />
-        <DownloadButton
-          onClickCallback={() => {
-            clipboard.copy(`${getBaseUrl()}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`)
-            toast.success('Copied direct link to clipboard.')
-          }}
-          btnColor="pink"
-          btnText={'Copy direct link'}
-          btnIcon="copy"
-          btnTitle={'Copy the permalink to the file to the clipboard'}
-        />
-        <DownloadButton
-          onClickCallback={() => setMenuOpen(true)}
-          btnColor="teal"
-          btnText={'Customise link'}
-          btnIcon="pen"
-        />
       </div>
     </>
   )
