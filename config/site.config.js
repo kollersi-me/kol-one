e0c837d6e3082495dc5580c2df9dfaf74fb1a85c
KOLLERSI <126153408+kollersi-me@users.noreply.github.com>
2024-11-06 22:56:45 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,14 +7,14 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your worker's environment variable if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'tuan.ngo@office.kollersi.com',
+  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'manga@office.kollersi.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/kollersi_logo_2024.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Kollersi's Drive",
+  title: "Kollersi's MANGA",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: '/KOLLERSI/',
@@ -49,7 +49,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'KOLLERSI HUB',
+      name: 'KOLLERSI MANGA',
       link: 'https://kollersi.com',
     },
   ],
